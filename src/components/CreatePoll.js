04d4dbdbb2c8f04520f1f29c2dccb7a3d979db99
Autry <JAutry@pnf.com>
2022-06-22 11:51:09 -0400

    Add CreatePoll functionality

--- src/components/CreatePoll.js
+++ src/components/CreatePoll.js
@@ -1,20 +1,69 @@
+import { handleAddQuestion } from "../actions/questions";
+import { connect } from "react-redux/es/exports";
+import { useState } from "react";
+import {useNavigate} from "react-router-dom";
+import {generateUID} from "../_DATA.js";
 
 const CreatePoll = (props) => {
+    const authedUser = props.authedUser;
+
+    const nav = useNavigate();
+    const [question, setQuestion] = useState("");
+    const [optionOne, setOptionOne] = useState("");
+    const [optionTwo, setOptionTwo] = useState("");
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (question.length > 0 && optionOne.length > 0 && optionTwo.length > 0) {
+            const questionId = generateUID();
+            const question = 
+            {
+                id: questionId,
+                author: authedUser,
+                optionOneText: optionOne,
+                optionTwoText: optionTwo,
+                timestamp: Date.now(),
+            }
+        
+
+            props.dispatch(handleAddQuestion(question));
+            nav("/");
+            console.log(question);
+        }
+    }
+
+
+    const handleQuestion = (e) => {
+        e.preventDefault();
+        setQuestion(e.target.value);
+    }
+
+    const handleOptionOne = (e) => {
+        e.preventDefault();
+        setOptionOne(e.target.value);
+    }
+
+    const handleOptionTwo = (e) => {
+        e.preventDefault();
+        setOptionTwo(e.target.value);
+    }
+
     return(
         <div>
             <h1>Create Poll</h1>
-            <form>
+            <form  onSubmit={handleSubmit}>
                 <div>
                     <label>Question:</label>
-                    <input type="text" name="question" />
+                    <input type="text" value = {question} name="question" onChange={handleQuestion}/>
                 </div>
                 <div>
                     <label>Option One:</label>
-                    <input type="text" name="optionOne" />
+                    <input type="text" value = {optionOne} name="optionOne" onChange={handleOptionOne}/>
                 </div>
                 <div>
                     <label>Option Two:</label>
-                    <input type="text" name="optionTwo" />
+                    <input type="text" value={optionTwo} name="optionTwo" onChange={handleOptionTwo}/>
                 </div>
                 <div>
                     <button type="submit">Submit</button>
@@ -24,4 +73,11 @@ const CreatePoll = (props) => {
     );
 }
 
-export default CreatePoll;
+function mapStateToProps(state) {
+    return {
+        authedUser: state.authedUser,
+    };
+}
+
+
+export default connect(mapStateToProps)(CreatePoll);
