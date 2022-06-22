9078c1e61a6282f41f478dc77c8bfd0823266bc3
Autry <JAutry@pnf.com>
2022-06-22 13:13:34 -0400

    Leaderboard displays each users questions and answers

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,16 +1,29 @@
+import { connect } from "react-redux/es/exports";
 
 const Leaderboard = (props) => {
+    let users = props.users;
+    let usersArray = Object.keys(users).map(key => users[key]);
+    let sortedUsers = usersArray.sort((a, b) => b.score - a.score);
+    console.log(usersArray)
     return (
         <div className="leaderboard">
         <h1>Leaderboard</h1>
         <ul>
-            <li>
-                <h2>1. John Doe</h2>
-                <p>Votes: 100</p>
+           {sortedUsers.map((user, index) => (
+
+            <li key={index}>
+                <p>{`${user.name} ${user.avatar} Questions: ${user.questions.length} Answers: ${Object.keys(user.answers).length}`}</p>
             </li>
+              ))}
         </ul>
         </div>
     );
 }
 
-export default Leaderboard;
+const mapStateToProps = (state) => {
+    return {
+        users: state.users
+    }
+}
+
+export default connect(mapStateToProps)(Leaderboard);
