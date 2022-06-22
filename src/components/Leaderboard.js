3a318278aa4c179aa96ceb08cc39f342eddbd4c1
Autry <JAutry@pnf.com>
2022-06-22 14:08:27 -0400

    Leadboard will update when a new question is created

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -4,15 +4,17 @@ const Leaderboard = (props) => {
     let users = props.users;
     let usersArray = Object.keys(users).map(key => users[key]);
     let sortedUsers = usersArray.sort((a, b) => b.score - a.score);
-    console.log(usersArray)
+
+    let questions = props.questions;
+    let questionsArray = Object.keys(questions).map(key => questions[key]);
+    
     return (
         <div className="leaderboard">
         <h1>Leaderboard</h1>
         <ul>
            {sortedUsers.map((user, index) => (
-
             <li key={index}>
-                <p>{`${user.name} ${user.avatar} Questions: ${user.questions.length} Answers: ${Object.keys(user.answers).length}`}</p>
+                <p>{`${user.name} ${user.avatar} Questions: ${questionsArray.filter(question => question.author === user.id).length} Answers: ${Object.keys(user.answers).length}`}</p>
             </li>
               ))}
         </ul>
@@ -22,7 +24,8 @@ const Leaderboard = (props) => {
 
 const mapStateToProps = (state) => {
     return {
-        users: state.users
+        users: state.users,
+        questions: state.questions
     }
 }
 
