b08a9a3a86a349c7e2cb4fd69dd461253cafe7af
Autry <JAutry@pnf.com>
2022-06-22 08:50:20 -0400

    Add Leaderboard component

---
+++ src/components/Leaderboard.js
@@ -0,0 +1,16 @@
+
+const Leaderboard = (props) => {
+    return (
+        <div className="leaderboard">
+        <h1>Leaderboard</h1>
+        <ul>
+            <li>
+                <h2>1. John Doe</h2>
+                <p>Votes: 100</p>
+            </li>
+        </ul>
+        </div>
+    );
+}
+
+export default Leaderboard;
