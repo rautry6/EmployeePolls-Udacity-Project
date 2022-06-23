d401e2f42c02cf34edd544f64090505539b50aac
Autry <JAutry@pnf.com>
2022-06-23 12:19:28 -0400

    Create PageNotFound component

---
+++ src/components/PageNotFound.js
@@ -0,0 +1,10 @@
+const PageNotFound = () => {
+  return (
+    <div>
+      <h1 className="page-not-found"> 404 Error</h1>
+      <h1>Page Not Found</h1>
+      <p>Sorry, this page does not exist.</p>
+    </div>
+  );
+};
+export default PageNotFound;
