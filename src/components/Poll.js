5ad22aff500ac7cac4e3e1e0f1f076bcc2bcc37e
Autry <JAutry@pnf.com>
2022-06-23 13:15:35 -0400

    Added poll details

--- src/components/Poll.js
+++ src/components/Poll.js
@@ -7,10 +7,37 @@ import PleaseLogin from "./PleaseLogin";
 const Poll = (props) => {
   let question = props.question;
   const [authedUser, setAuthedUser] = useState(null);
+  const [answered, setAnswered] = useState(props.users[props.authedUser].answers[question.id]);
+
+  let questionOneAnswers = question.optionOne.votes.length;
+  let questionTwoAnswers = question.optionTwo.votes.length;
+
+  let questionOnePercentage =
+    (questionOneAnswers / (question.optionOne.votes.length + question.optionTwo.votes.length)) * 100;
+  let questionTwoPercentage =
+    (questionTwoAnswers / (question.optionTwo.votes.length + question.optionOne.votes.length))* 100;
 
   useEffect(() => {
     setAuthedUser(props.authedUser);
-  }, [props.authedUser]);
+    if (authedUser) {
+      if (props.users[props.authedUser].answers[question.id]) {
+        setAnswered(true);
+        if (
+          props.users[props.authedUser].answers[question.id] === "optionOne"
+        ) {
+          if (question.optionOne.votes.includes(props.authedUser)) {
+            return;
+          }
+          question.optionOne.votes.push(props.authedUser);
+        } else {
+          if (question.optionTwo.votes.includes(props.authedUser)) {
+            return;
+          }
+          question.optionTwo.votes.push(props.authedUser);
+        }
+      }
+    }
+  }, [props.authedUser, props.users[props.authedUser].answers]);
 
   const handleOptionOne = (e) => {
     e.preventDefault();
@@ -38,6 +65,14 @@ const Poll = (props) => {
                   value="Choose"
                   onClick={handleOptionOne}
                 />
+                {answered ? (
+                  <div>
+                    <p>{`Answers: ${questionOneAnswers}`}</p>
+                    <p>{`Percentage: ${questionOnePercentage.toFixed(2)}%`}</p>
+                  </div>
+                ) : (
+                  <p></p>
+                )}
               </div>
               <div>
                 <p>{question.optionTwo.text}</p>
@@ -47,6 +82,14 @@ const Poll = (props) => {
                   value="Choose"
                   onClick={handleOptionTwo}
                 />
+                {answered ? (
+                  <div>
+                    <p>{`Answers: ${questionTwoAnswers}`}</p>
+                    <p>{`Percentage: ${questionTwoPercentage.toFixed(2)}%`}</p>
+                  </div>
+                ) : (
+                  <p></p>
+                )}
               </div>
             </form>
           </ul>
@@ -62,6 +105,7 @@ function mapStateToProps(state, { id }) {
   return {
     question: state.questions[id],
     authedUser: state.authedUser,
+    users: state.users,
   };
 }
 
