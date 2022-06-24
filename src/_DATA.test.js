83c53084e030b53317849f1bb8ab3b9a6fb93fed
Autry <JAutry@pnf.com>
2022-06-24 09:42:00 -0400

    Add saveQuestionAnswer unit tests

--- src/_DATA.test.js
+++ src/_DATA.test.js
@@ -1,31 +1,59 @@
-import { _saveQuestion } from "./_DATA";
+import { _saveQuestion, _saveQuestionAnswer } from "./_DATA";
 
-let saveQuestion = _saveQuestion
+let saveQuestion = _saveQuestion;
+let saveQuestionAnswer = _saveQuestionAnswer;
 
 describe("_saveQuestion", () => {
-    it("returns a question", async () => {
-        const question = {
-            id: "test-id",
-            timestamp: Date.now(),
-            author: "test-author",
-            optionOneText: "test-optionOneText",
-            optionTwoText: "test-optionTwoText",
-        }
-        const result = await saveQuestion(question)
-        console.log(result)
-        expect(result).toEqual(expect.objectContaining({
-            id: expect.any(String),
-            timestamp: expect.any(Number),
-            author: expect.any(String),
-           optionOne : expect.objectContaining({
-                text: expect.any(String),
-                votes: expect.arrayContaining([])
-            }),
-            optionTwo : expect.objectContaining({
-                text: expect.any(String),
-                votes: expect.arrayContaining([])
-            }),
-        }))
-    })
+  it("returns a question", async () => {
+    const question = {
+      id: "test-id",
+      timestamp: Date.now(),
+      author: "test-author",
+      optionOneText: "test-optionOneText",
+      optionTwoText: "test-optionTwoText",
+    };
+    const result = await saveQuestion(question);
+    expect(result).toEqual(
+      expect.objectContaining({
+        id: expect.any(String),
+        timestamp: expect.any(Number),
+        author: expect.any(String),
+        optionOne: expect.objectContaining({
+          text: expect.any(String),
+          votes: expect.arrayContaining([]),
+        }),
+        optionTwo: expect.objectContaining({
+          text: expect.any(String),
+          votes: expect.arrayContaining([]),
+        }),
+      })
+    );
+  });
 
-})
+  it("throws an error if invalid question", async () => {
+    const question = {
+      id: "test-id",
+      timestamp: Date.now(),
+    };
+    const error = await saveQuestion(question).catch((e) => e);
+    expect(error).toEqual(
+      "Please provide optionOneText, optionTwoText, and author"
+    );
+  });
+});
+
+describe("_saveQuestionAnswer", () => {
+  it("returns true if correctly formatted data is passed", async () => {
+    const authedUser = "sarahedo";
+    const qid = "6ni6ok3ym7mf1p33lnez";
+    const answer = "optionTwo";
+    const result = await saveQuestionAnswer({ authedUser, qid, answer });
+    expect(result).toEqual(true);
+  });
+  it("throws an error if invalid data is passed", async () => {
+    const authedUser = "test-authedUser";
+    const id = "test-id";
+    const error = await saveQuestionAnswer({ authedUser, id }).catch((e) => e);
+    expect(error).toEqual("Please provide authedUser, qid, and answer");
+  });
+});
