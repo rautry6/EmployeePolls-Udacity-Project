6c3aafd2248fe0d7253605c3f4a99a32f50e8804
Autry <JAutry@pnf.com>
2022-06-27 14:05:12 -0400

    Add LoginPage css

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,3 +1,4 @@
+import "./Login.css"
 import { connect } from "react-redux";
 import { useState } from "react";
 import { setAuthedUser } from "../actions/authedUser.js";
@@ -39,28 +40,28 @@ const LoginPage = (props) => {
 
   return (
     <div className="login-page">
-      <h1>Employee Polls</h1>
-      <h2>Login</h2>
-      <div>
+       <h1 className="login-title">Employee Polls</h1>
+      <h2 className="login-header">Login</h2>
+      <div className="login-field">
         {error ? (
           <div>
-            <h3 itemID="invalid-login">Invalid username or password</h3>{" "}
-            <p>User</p>
-            <input type="text" value={username} onChange={handleUsername} />
-            <p>Password</p>
-            <input type="password" value={password} onChange={handlePassword} />
+            <h3 className="invalid-login" data-testid = "failed-login-text">Invalid username or password</h3>{" "}
+            <p className="username">Username</p>
+            <input type="text" value={username} onChange={handleUsername} data-testid = "failed-login-input" className="login-input"/>
+            <p className="password">Password</p>
+            <input type="password" value={password} onChange={handlePassword} data-testid = "failed-password-input" className="password-input"/>
             <p>
-              <button onClick={handleSubmit}>Login</button>
+              <button onClick={handleSubmit} data-testid = "failed-submit-button" className="submit-button">Login</button>
             </p>
           </div>
         ) : (
           <div>
-            <p>User</p>
-            <input type="text" value={username} onChange={handleUsername} />
-            <p>Password</p>
-            <input type="password" value={password} onChange={handlePassword} />
+            <p className="username">Username</p>
+            <input type="text" value={username} onChange={handleUsername} data-testid = "login-input" className="login-input"/>
+            <p  className="password">Password</p>
+            <input type="password" value={password} onChange={handlePassword} data-testid = "password-input" className="password-input"/>
             <p>
-              <button onClick={handleSubmit}>Login</button>
+              <button onClick={handleSubmit} data-testid = "submit-button" className="submit-button">Login</button>
             </p>
           </div>
         )}
